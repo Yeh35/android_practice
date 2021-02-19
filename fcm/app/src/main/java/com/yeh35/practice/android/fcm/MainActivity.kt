0ed597285520bc0dff77f7293bd2dec0e9f4c983
jaws0227 <jaws0910@gmail.com>
2021-02-19 17:40:01 +0900

    FCM 연습 추가

--- fcm/app/src/main/java/com/yeh35/practice/android/fcm/MainActivity.kt
+++ fcm/app/src/main/java/com/yeh35/practice/android/fcm/MainActivity.kt
@@ -2,10 +2,36 @@ package com.yeh35.practice.android.fcm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
     }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
 }
---
+++ fcm/app/src/main/java/com/yeh35/practice/android/fcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,57 @@
+package com.yeh35.practice.android.fcm
+
+import android.annotation.SuppressLint
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    @SuppressLint("LongLogTag")
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.from)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+
+            if ( /* Check if data needs to be processed by long running job */true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+//                scheduleJob()
+            } else {
+                // Handle message within 10 seconds
+//                handleNow()
+            }
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body)
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+
+    @SuppressLint("LongLogTag")
+    fun onTokenRefresh() {
+        // Get updated InstanceID token.
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d(TAG, "Refreshed token: $refreshedToken")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(refreshedToken)
+    }
+
+    companion object {
+        const val TAG = "MyFirebaseMessagingService"
+    }
+}
