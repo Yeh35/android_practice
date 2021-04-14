39aacf0d1fd4012c91c7097bba86032c3f5eddc9
jaws0227 <jaws0910@gmail.com>
2021-04-14 11:40:50 +0900

    androidTest 내용 추가

--- testing/app/src/androidTest/java/com/yeh35/practice/android/testing/ExampleInstrumentedTest.kt
+++ testing/app/src/androidTest/java/com/yeh35/practice/android/testing/ExampleInstrumentedTest.kt
@@ -15,10 +15,13 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.yeh35.practice.android.testing", appContext.packageName)
+
+
     }
 }
--- testing/app/src/main/java/com/yeh35/practice/android/testing/MainActivity.kt
+++ testing/app/src/main/java/com/yeh35/practice/android/testing/MainActivity.kt
@@ -2,11 +2,17 @@ package com.yeh35.practice.android.testing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvHello = findViewById<TextView>(R.id.tv_hello)
+        tvHello.setOnClickListener {
+            tvHello.text = "on! Click"
+        }
     }
 
     fun getName(): String {
